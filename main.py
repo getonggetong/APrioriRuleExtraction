c471ed1cb333783ecda06d32a529c579bcb2676f
i3wangyi <i3.wangyi@gmail.com>
2014-04-28 18:33:53 -0400

    algorithm

--- main.py
+++ main.py
@@ -1,99 +1,42 @@
-import csv
-
-# global dictionary
-dic = {}
-csvfname = 'DYCD_after-school_programs.csv'
-num_records = 0
-csvreader = None 
-result = []
-
-####### functions #######
-
-def readCSV():
-	return open(csvfname)
-
-def buildDic(csvfile):
-	global dic
-	global num_records
-	global csvreader 
-	csvreader = csv.reader(csvfile.read().splitlines(), delimiter=",")
-	for row in csvreader:
-		# caution: a row doesn't contain same item more than once
-		num_records += 1
-		for item in row:
-			if(item not in dic.keys()):
-				dic[item] = 1
-			else:
-				dic[item] = dic[item]+1
+#!/usr/bin/python
 
 def apriori_gen(L):
 	# Join
 	
 	# Prune
-
 	pass	
 
+def prune(key):
+	pass
+
+
+def diff(a,b):
+	for i in range(len(a)-1):
+		if(a[i]!=b[i]):
+			return False
+	if(a[len(a)-1] < b[len(a) - 1]):
+		return True
+
+def merge(L,oldwords,word):
+	global L1
+	oldList = L.get(oldwords)
+	tmpList = L1.get(word)
+	return list(set(oldList+tmpList)) 
+
 def Apriori(L):
-	global reader
-	global result
+	C = {};
+	for k1 in L.keys():
+		p = k1.split(",")
+		for k2 in L.keys():
+			q = k2.split(",")
+			if(p!=q):
+				if(diff(p,q)):
+					key = p
+					key.append(q[len(q)-1])
+					if(prune(key)):
+						C[key] = merge(L,p,q[len(q)-1])
 
-	# nL = L
-	# while nL!=None:
-	# 	C = apriori_gen(nL)
+L1 = {"a":[0,3],"b":[0,1],"c":[0,1,2],"d":[0,1,2,3]}
+L2 = {"a,c":[0,2], "a,b":[0,3],"a,d":[0,2,3],"b,d":[0,1,3],"c,d":[0,1,2],"b,c":[0,1]}
 
-	# 	for row in reader:
-	 		# Find all the supported itemsets with size k in C
-			# C_t = subset(C_k, t)
-			# forall candidates c \in C_t
-				# c.count++
-		# end
-
-		# nL = {c \in C_k | c.count >= min_sup}
-	# end
-
-	# return 
-
-def filter(threshold, L):
-	global num_records
-	global csvreader
-	Fl = []
-	for row in csvreader:
-		# if this row contains 
-		pass
-
-
-def supportFilter(sup_thre):
-	#according to the threshold, filter the dictionary 
-	global dic
-	global num_records
-
-	FilteredDict = []
-
-	for key in dic.keys():
-		dic[key] = dic[key]/num_records
-		if(dic[key] >= sup_thre):
-			FilteredDict.append(key)
-
-	# Ensure it's sorted
-	return FilteredDict.sort()
-
-
-# get input 
-
-def main():
-	global num_records
-	min_sup = 0.5
-	#readCSV
-	f = readCSV()
-	#for each row, count the words and its frequency 
-	buildDic(f)
-	# Generate L_1
-	L = filter(min_sup, dic)
-
-	Apriori(L)
-	#calculate the support
-
-
-#Entry
-if __name__ == "__main__":
-	main()
+Apriori(L2)
