a3e21be0df82f794e7b770c03dd5c1b98387017e
i3wangyi <i3.wangyi@gmail.com>
2014-04-28 18:35:27 -0400

    algorithm

--- main.py
+++ main.py
@@ -1,42 +1,70 @@
-#!/usr/bin/python
+import csv
 
-def apriori_gen(L):
-	# Join
-	
-	# Prune
-	pass	
+csvFileName = 'DYCD_after-school_programs.csv'
+numberRecords = 0
+min_sup = 0.4
+min_conf = 0.9
+LList = []#list of L1,L2,...,Lk
 
-def prune(key):
-	pass
+#build initial dictionary
+def initDict(file):
+    #define a temporary dictionary to calculate the number of appearance of EACH ITEM <item, list of appearing records no.>
+    initDict = {}
+    global numberRecords
+    
+    csvreader = csv.reader(file.read().splitlines(), delimiter=",")
+    for row in csvreader:
+        #increase the number of records by one for further support/confidence calculation 
+        numberRecords += 1
+        #
+        for item in row:
+            if(item not in initDict.keys()):
+                initDict[item] = [numberRecords]
+            else:
+                initDict[item].append(numberRecords)
+    
+    return initDict
+    
+    
+def aprioriAlgo():
+    global LList
+    L1 = LList[0]
+    #for (k = 2; Lk_1 != null; k++):
+    k = 2
+    while len(LList) >= k - 1:
+        #the number of L and the position it is stored in LList has a difference 1. e.g. L2 is stored in LList[1]
+        Lk_1 = LList[k - 1 - 1]
+        Ck = apriori_gen(Lk_1)#get new candidates set [<"pen, int", [1,2,6,9,56,...]>,<"...", [....]>,<...>,...]
+        # for each itemset in Ck
+
+        k += 1
 
 
-def diff(a,b):
-	for i in range(len(a)-1):
-		if(a[i]!=b[i]):
-			return False
-	if(a[len(a)-1] < b[len(a) - 1]):
-		return True
 
-def merge(L,oldwords,word):
-	global L1
-	oldList = L.get(oldwords)
-	tmpList = L1.get(word)
-	return list(set(oldList+tmpList)) 
+def main():
+    global LList
+    global csvFileName
+    global min_sup
+    global numberRecords
 
-def Apriori(L):
-	C = {};
-	for k1 in L.keys():
-		p = k1.split(",")
-		for k2 in L.keys():
-			q = k2.split(",")
-			if(p!=q):
-				if(diff(p,q)):
-					key = p
-					key.append(q[len(q)-1])
-					if(prune(key)):
-						C[key] = merge(L,p,q[len(q)-1])
+    #open CSV file
+    file = open(csvFileName)
+    #read the file and build a 1-element-key dictionary with value be the number of appearance
+    dictionary = initDict(file)
+    #get L1 from C1
+    L1 = {}
+    for item in dictionary.keys():
 
-L1 = {"a":[0,3],"b":[0,1],"c":[0,1,2],"d":[0,1,2,3]}
-L2 = {"a,c":[0,2], "a,b":[0,3],"a,d":[0,2,3],"b,d":[0,1,3],"c,d":[0,1,2],"b,c":[0,1]}
+        # check if support meets min_sup
+        if float(len(dictionary[item])) / numberRecords >= min_sup:
+            L1[item] = dictionary[item]
+    #add L1 to the L list
+    LList.append(L1)
+    print L1
 
-Apriori(L2)
+    # aprioriAlgo()
+
+
+    
+if __name__ == "__main__":
+    main()
